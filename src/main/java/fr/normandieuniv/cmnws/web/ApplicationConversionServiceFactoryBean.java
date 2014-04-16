e02afe85e125fa7733adc0b75f016ece4aa2ac2a
Vincent Bonamy <vbonamy@gmail.com>
2014-04-16 17:52:14 +0200

    nettoyage

--- src/main/java/fr/normandieuniv/cmnws/web/ApplicationConversionServiceFactoryBean.java
+++ src/main/java/fr/normandieuniv/cmnws/web/ApplicationConversionServiceFactoryBean.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package fr.normandieuniv.cpmnws.web;
+package fr.normandieuniv.cmnws.web;
 
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.support.FormattingConversionServiceFactoryBean;
