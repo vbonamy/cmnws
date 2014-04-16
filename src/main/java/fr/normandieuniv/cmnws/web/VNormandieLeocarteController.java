a7be57adf650c67bf05f7a01121fe751929041e3
Vincent Bonamy <vbonamy@gmail.com>
2014-04-16 17:46:59 +0200

    Vues finder ...

--- src/main/java/fr/normandieuniv/cmnws/web/VNormandieLeocarteController.java
+++ src/main/java/fr/normandieuniv/cmnws/web/VNormandieLeocarteController.java
@@ -2,14 +2,21 @@ package fr.normandieuniv.cmnws.web;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.roo.addon.web.mvc.controller.finder.RooWebFinder;
+import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import fr.normandieuniv.cmnws.db.VNormandieLeocarte;
+
 @RequestMapping("/vnormandieleocarte/**")
 @Controller
+@RooWebScaffold(path = "vnormandieleocarte", formBackingObject = VNormandieLeocarte.class, create = false, delete = false, update = false)
+@RooWebFinder
 public class VNormandieLeocarteController {
 
     @RequestMapping(method = RequestMethod.POST, value = "{id}")
